0ecbf281dd88e4b6ab35dc96592f2ac1360b2913
Brian <brianheise22@gmail.com>
2025-08-13 13:30:02 -0400

    change paragraph on index

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require_once __DIR__ . '/includes/header.php';
 $title = 'Home';
 ?>
 <h2>Welcome to Daytona Supply</h2>
-<p>Your trusted supplier for quality parts and supplies. Explore our catalogue and place purchase orders online. If you are new here, please create an account to get started.</p>
+<p>Your trusted supplier for shipping and janitorial supplies. Explore our catalogue and place purchase orders online. If you are new here, please create an account to get started.</p>
 
 <?php include __DIR__ . '/includes/footer.php'; ?>
